01aceb7edb22078d259fc052d68047f430dfb012
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2018-11-19 16:28:21 +0300

    Release

--- src/Task_120.java
+++ src/Task_120.java
@@ -1,32 +1,31 @@
-import java.io.FileNotFoundException;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Formatter;
-import java.util.FormatterClosedException;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Task_120 {
 	public static void main(String[] args) {
-		Area test = new Area();
-		try(final Formatter output = new Formatter("OUTPUT.TXT")) {
-			output.format(test.toString());
-		}catch (FileNotFoundException | FormatterClosedException e) {
+		final String outputPath = "OUTPUT.TXT";
+		final Area test = new Area();
+		try(final BufferedWriter output = Files.newBufferedWriter(Paths.get(outputPath))) {
+			output.write(test.toString());
+		}catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 }
 
-class Area {
+//-----------------------------------------------------------------------------
+/*public*/ class Area {
+	//-----------------------------------------------------------------------------fields
 	private int[][] area;
 	private int rows;
 	private int columns;
-
-	//-----------------------------------------------------------------------------
-	public Area(String path)
+	//-----------------------------------------------------------------------------constructors
+	/*public*/ private Area(String path)
 	{
 		try(final Scanner input = new Scanner(Paths.get(path))) {
-			//-----------------------------------------------------------------------------
 			if(input.hasNext()) {
 				//-----------------------------------------------------------------------------
 				this.rows = input.nextInt(); //read data from file
@@ -37,38 +36,41 @@ class Area {
 						this.area[row][column] = input.nextInt(); //read data from file
 					}
 				}
-
-
+				//-----------------------------------------------------------------------------
+				//function from the Internet, but I understood how it work's
 				for(int row = 0; row < this.rows; row++){
 					for(int column = 0; column < this.columns; column++){
-						if(row>0 && column>0){
-							this.area[row][column]+=Math.min(this.area[row-1][column], this.area[row][column-1]);
+						if((row > 0) && (column > 0)){
+							this.area[row][column]+= Math.min(this.area[row - 1][column], this.area[row][column - 1]);
 						}
 						else{
-							if(row>0){
-								this.area[row][column]+=this.area[row-1][column];
-							}else if(column>0){
-								this.area[row][column]+=this.area[row][column-1];
+							if(row > 0){
+								this.area[row][column]+= this.area[row - 1][column];
+							}
+							else if(column > 0){
+								this.area[row][column]+= this.area[row][column - 1];
 							}
 						}
 					}
 				}
+				//-----------------------------------------------------------------------------
 			}
 			//-----------------------------------------------------------------------------
 			else {
 				throw new IOException("File is empty!");
 			}
 			//-----------------------------------------------------------------------------
-		}catch (IOException | NoSuchElementException e) {
+		}catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public Area()
+	/*public*/ Area()
 	{
 		this("INPUT.TXT");
 	}
-
+	//-----------------------------------------------------------------------------
+	//-----------------------------------------------------------------------------methods
 	@Override
 	public String toString()
 	{
